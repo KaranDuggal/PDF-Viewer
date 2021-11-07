920a1f72d185bde13ee47cddab11064ec0e23423
duggalkaran <duggalkaran400@gmail.com>
2021-11-07 09:31:08 +0530

    pdf open sussessfully

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String url = "http://africau.edu/images/default/sample.pdf";
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
   @override
   void initState() {
     super.initState();
@@ -16,9 +19,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(),
-        body: const Text("data")
+      appBar: AppBar(
+        title: const Text('PDF Viewer App'),
+      ),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        key: _pdfViewerKey,
+      ),
+      // body: SfPdfViewer.network(
+      //   'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+      //   key: _pdfViewerKey,
+      // ),
     );
   }
 }
--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,6 +1,5 @@
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
-import 'package:kbc_game/src/utils/constant.dart';
+import 'package:pdf_viewer/src/utils/constant.dart';
 class ApiService{
   get(_url) async {
     // print('${MyConstant.localBaseURL}$_url');
@@ -8,8 +7,4 @@ class ApiService{
     return(apiData.body);
   }
 
-}
-class GoogleSignInApi{
-  static final _googleSignIn = GoogleSignIn();
-  static Future<GoogleSignInAccount?> login()=> _googleSignIn.signIn();
 }
