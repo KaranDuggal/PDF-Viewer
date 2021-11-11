5248176be54311aff782388f8b81204be9b5b435
KaranDuggal <duggalkaran400@gmail.com>
2021-11-11 14:43:03 +0530

    about us card

--- lib/src/screens/about_us_screen.dart
+++ lib/src/screens/about_us_screen.dart
@@ -4,8 +4,55 @@ class AboutUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("About Us"),
+    return Center(
+      child: Card(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width - 100,
+          height: MediaQuery.of(context).size.width - 180,
+          child: Column(
+            children: [
+              const SizedBox(height: 20,),
+              const Text("About Us",style: TextStyle(fontSize: 26),),
+              const SizedBox(height: 20,),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text("Developer Name" ,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                      Text("Karan Duggal",style: TextStyle(fontSize: 16),)
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40,),
+              ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                minimumSize: const Size(200, 50),
+              ),
+              onPressed: ()async {                
+              },
+              child:  SizedBox(
+                width: 120,
+                child: Row( 
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Icon(Icons.link),
+                    Text("Git Hub Link"),
+                  ],
+                ),
+              )
+            ),
+            ],
+          )
+        ),
+        elevation: 8,
+        shadowColor: Colors.blue,
+        // shape: BeveledRectangleBorder(
+        //     borderRadius: BorderRadius.circular(15)
+        // ),
+      ),
     );
   }
 }
